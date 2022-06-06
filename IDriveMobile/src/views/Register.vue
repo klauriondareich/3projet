438c4be8d47856a6d129ea03db3fba3bb44e9c32
laurion <klauriondareich@gmail.com>
2022-06-06 18:36:17 +0200

    Register vue and logic done

--- IDriveMobile/src/views/Register.vue
+++ IDriveMobile/src/views/Register.vue
@@ -4,19 +4,23 @@
       <div id="img">
           <ion-img src="/assets/new-message.png"></ion-img>
       </div>
+      <div>
+        <p class="error-msg">{{errorMessage}}</p>
+         <p class="success-msg">{{successMessage}}</p>
+      </div>
        <ion-item class="form-marg">
         <ion-label position="floating">Nom d'utilisateur</ion-label>
-        <ion-input type="text"></ion-input>
+        <ion-input type="text" v-model="username"></ion-input>
       </ion-item>
       <ion-item class="form-marg">
         <ion-label position="floating">Adresse email</ion-label>
-        <ion-input type="email"></ion-input>
+        <ion-input type="email" v-model="email"></ion-input>
       </ion-item>
       <ion-item class="form-marg">
         <ion-label position="floating">Mot de passe</ion-label>
-        <ion-input type="password"></ion-input>
+        <ion-input type="password" v-model="password"></ion-input>
       </ion-item>
-       <ion-button shape="round" expand="block" type="submit" id="register">S'inscrire</ion-button>
+       <ion-button shape="round" expand="block" type="submit" id="register" @click="registerUser()">S'inscrire</ion-button>
        <ion-button id="login" href="/user/login">Se connecter</ion-button>
     </ion-content>
   </ion-page>
@@ -26,6 +30,7 @@
 
 import { defineComponent } from 'vue';
 import { IonPage, IonImg, IonContent, IonButton, IonLabel, IonInput, IonItem } from '@ionic/vue';
+import axios from 'axios'
 
 export default defineComponent({
   name: 'RegisterPage',
@@ -37,6 +42,42 @@ export default defineComponent({
     IonLabel,
     IonInput, 
     IonItem
+  },
+  data(){
+    return {
+      username: null,
+      email: null,
+      password: null,
+      errorMessage: "",
+      successMessage: ""
+    }
+  },
+
+  methods:{
+
+    // This func register user in db
+    registerUser(){
+
+     axios.post('http://localhost:3000/api/v1/register', {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        size_of_all_docs: 0,
+        blocked: false
+    }, {headers: {'Access-Control-Allow-Origin': '*'}})
+    .then( (response) => {
+      if (response.status == 200){
+        this.errorMessage = "Votre compte a été crée avec succès!";
+        return;
+      }
+      this.errorMessage = response.data;
+    })
+    .catch( (error) => {
+      if (error.response.status == 400){
+        this.errorMessage = error.response.data;
+      }});
+
+    }
   }
 });
 </script>
@@ -68,5 +109,13 @@ div#img{
   width: 250px;
   height: 250px;
   margin: auto;
+}
+.error-msg{
+  color: red;
+  padding-left: 30px;
+}
+.success-msg{
+  color: green;
+  padding-left: 30px;
 }
 </style>
