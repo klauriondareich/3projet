21c7f33894fd717ea9da90fa4d3d2d8feb089802
laurion <klauriondareich@gmail.com>
2022-06-07 02:16:05 +0200

    Login feature implemented

--- IDriveMobile/src/views/Login.vue
+++ IDriveMobile/src/views/Login.vue
@@ -4,15 +4,18 @@
       <div id="img">
           <ion-img src="/assets/welcome.png"></ion-img>
       </div>
+       <div>
+        <p class="error-msg">{{errorMessage}}</p>
+      </div>
       <ion-item class="form-marg">
         <ion-label position="floating">Adresse email</ion-label>
-        <ion-input type="email"></ion-input>
+        <ion-input type="email" v-model="email"></ion-input>
       </ion-item>
       <ion-item class="form-marg">
         <ion-label position="floating">Mot de passe</ion-label>
-        <ion-input type="password"></ion-input>
+        <ion-input type="password" v-model="password"></ion-input>
       </ion-item>
-       <ion-button shape="round" expand="block" id="login" href="auth/home">Se connecter</ion-button>
+       <ion-button shape="round" expand="block" id="login" @click="loginUser()">Se connecter</ion-button>
        <ion-button id="register" type="submit" href="/user/register">S'inscrire</ion-button>
     </ion-content>
   </ion-page>
@@ -22,6 +25,7 @@
 
 import { defineComponent } from 'vue';
 import { IonPage, IonImg, IonContent, IonButton, IonLabel, IonInput, IonItem } from '@ionic/vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'LoginPage',
@@ -35,14 +39,39 @@ export default defineComponent({
     IonItem
   },
 
-  setup(){
-    
-    const loginUser = () => {
-      console.log("logget in")
-    };
+  data(){
+    return {
+      email: null,
+      password: null,
+      errorMessage: "",
+    }
+  },
+  
+  methods:{
 
-    return{
-      loginUser
+    // This func register user in db
+    loginUser(){
+
+      axios.post('http://localhost:3000/api/v1/login', {
+        email: this.email,
+        password: this.password,
+      }, {headers: {'Access-Control-Allow-Origin': '*'}})
+      .then( (response) => {
+        if (response.status == 200){
+          localStorage.setItem('auth-token', response.data);
+          let authToken = localStorage.getItem('auth-token');
+          if (authToken != "" || authToken != null){
+            this.$router.push("/auth/home")
+          }
+          return;
+        }
+        this.errorMessage = response.data;
+      })
+      .catch( (error) => {
+        if (error.response.status == 400){
+          this.errorMessage = error.response.data;
+        }});
+
     }
   }
 });
@@ -75,5 +104,9 @@ div#img{
   width: 250px;
   height: 250px;
   margin: auto;
+}
+.error-msg{
+  color: red;
+  padding-left: 30px;
 }
 </style>
