c42e8c9adaf7af44f48e04dc252a2bb58ac2f1d0
laurion <klauriondareich@gmail.com>
2022-06-09 12:06:52 +0200

    concat original filename with timestamp

--- IDriveMobile/backend/src/middleware/fileUpload.js
+++ IDriveMobile/backend/src/middleware/fileUpload.js
@@ -1,7 +1,7 @@
 import util from 'util';
 import multer from 'multer';
 
-let __basedir = "C:/Projets/3proj/i-drive-mobile/Pear";
+let __basedir = "C:/Projets/3proj/i-drive-mobile/IDriveMobile/public";
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
