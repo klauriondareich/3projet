6e004aaaf71c89c51d5a13a0ce26c11cd49548d5
laurion <klauriondareich@gmail.com>
2022-06-11 15:13:47 +0200

    config updates

--- IDriveMobile/backend/index.js
+++ IDriveMobile/backend/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './src/routes/appRoutes.js';
 import cors from 'cors';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +16,8 @@ app.use(cors({
 
 app.use(cors());
 
+
+
 // Body Parser config
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,4 +28,14 @@ app.get("/", (req, res) =>
     res.send(`Your server is running in PORT=${PORT}`)
 );
 
-app.listen(PORT, () => console.log("server is running"));
+// app.listen(PORT, () => console.log("server is running"));
+
+const options ={
+  key:fs.readFileSync('C:/Projets/3proj/i-drive-mobile/IDriveMobile/certif/key.pem'),
+  cert:fs.readFileSync(path.join('C:/Projets/3proj/i-drive-mobile/IDriveMobile/certif/cert.pem')) 
+}
+
+
+const sslserver =https.createServer(options,app)
+
+sslserver.listen(PORT,()=>{console.log(`Secure Server is listening on port ${PORT}`)});
