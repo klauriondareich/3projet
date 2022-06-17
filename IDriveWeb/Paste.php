5c809e26585cd19db607f224721e4f9f20b95a62
laurion <klauriondareich@gmail.com>
2022-06-17 12:26:50 +0200

    project name changed

---
+++ IDriveWeb/Paste.php
@@ -0,0 +1,27 @@
+<?php
+$session_id = $_GET["id"];
+$file = $_GET["file"];
+
+?>
+
+
+<?php
+if(isset($_REQUEST['destination']))
+{
+	$source = "../IDriveMobile/public/uploads/$file";
+	$destination = $_POST["destination"];
+
+	if(copy($source, "$destination/$file"))
+	{
+		echo "Fichier copié avec succès!";
+	}
+}
+?>
+
+<html><body>
+<form method="post" action="Paste.php?id=<?php echo $session_id?>&file=<?php echo $file?>">
+	Destination : <input type="text" name="destination">
+	<input type="submit">
+</form>
+</body>
+</html>
---
+++ IDriveWeb/Register.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Register</title>
+
+</head>
+
+<body>
+
+	<?php
+	require('Connexion/Connexion_db.php');
+
+	if(isset($_REQUEST['username']))
+	{
+		$username = $_REQUEST['username'];
+		$email = $_REQUEST['email'];
+		$mdp = $_REQUEST['mdp'];
+		$mdp = password_hash($mdp, PASSWORD_DEFAULT);
+
+		$sql_select = "SELECT * FROM users WHERE username=?";
+		$stmt_select = $connexion->prepare($sql_select);
+		$stmt_select->execute([$username]);
+		$user = $stmt_select->fetch();
+		if($user)
+		{
+			echo "Utilisateur existant!";
+		}
+		else
+		{
+			try {
+				$sql = "INSERT INTO users (username, email, mdp) VALUES (?,?,?)";
+				$stmt = $connexion->prepare($sql);
+				$stmt->execute([$username, $email, $mdp]);
+			}catch(PDOexception $e){
+				echo $sql . "</br>" . $e->getMessage();
+			}
+			if($stmt)
+			{
+				echo "<h3>Compte créé<h3></br><a href='Login.php'>Connectez vous</a>";
+			}
+		}
+	}
+	?>
+
+
+
+	<form class="register" action="Register.php" method="post">
+		<h1 class="register-title">Créez votre compte</h1>
+
+		<div id="div-register">
+			<label for="username">Nom d'utilisateur</label>
+			</br>
+			<input type="text" id="username" name="username">
+			</br>
+			<label for="email">Email</label>
+			</br>
+			<input type="email" id="email" name="email">
+			</br>
+			<label for="mdp">Mot de passe</label>
+			</br>
+			<input type="password" id="mdp" name="mdp">
+			</br>
+			<input type="submit" name="submit" value="Enregistrer" class="register-button">
+		</div>
+
+		<div id="already-account">
+			<p class="login-directly">Déjà membre ? </p>
+			<a href="Login.php">Connectez vous</a>
+		</div>
+	</form>
+
+</body>
+
+</html>
