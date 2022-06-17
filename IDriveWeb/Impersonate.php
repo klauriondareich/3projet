b1a292c8691dd14fa967088a4746214eeafc63ff
laurion <klauriondareich@gmail.com>
2022-06-17 23:49:17 +0200

    impersonate page designed

--- IDriveWeb/Impersonate.php
+++ IDriveWeb/Impersonate.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
+	<title>Impersonate User</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body>
+<body class="admin-body">
+	<div class="home-item">
+
 	<?php
 	
 	require("Connexion/Connexion_db.php");
@@ -13,7 +19,17 @@
 	
 	?>
 
-	<a href="Logout.php">Logout</a>
+	<?php include("shared/adminHeader.php")?> 
+
+	<table class="table-view">
+		<tr>
+			<th>Nom du fichier</th>
+			<th>Date d'ajout</th>
+			<th>Taille</th>
+			<th>Type de fichier</th>
+			<th>Preview</th>
+			<th>Actions</th>
+		</tr>
 
 	<?php
 	$sql_select = "SELECT * FROM docs WHERE user_id='$session_id'";
@@ -26,32 +42,25 @@
 			$upload_date = $row["upload_date"];
 			$size = $row["size"];
 			$file_type = $row["file_type"];
+			$file_path = $row["file_path"];
 			
-			
+			// <object data='C:/xampp/htdocs/3proj/IDriveMobile/public/uploads/$filename' width='300' height='200'></object> 
      
-			echo "<table>
-    <thead>
-        <tr>
-            <th colspan='2'>$filename</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-         <object data='C:/xampp/htdocs/3proj/IDriveMobile/public/uploads/$filename' width='300' height='200'></object> 
-        	<a href='uploads/$filename' target='_blank'>$filename</a>
-            <td>$upload_date</td>
-            <td>$size</td>
-            <td>$file_type</td>
-        </tr>
-    </tbody>
-    </table>
-    </br>
-    <a href='Paste.php?id=$session_id&file=$filename'>Copy and Paste</a>
-    	";
+			echo "
+			<tr>
+			
+				<td>$filename</td>
+				<td>$upload_date</td>
+				<td>$size</td>
+				<td>$file_type</td>
+				<td><a href='../IDriveMobile/public/uploads/$file_path'  class='imp-style' target='_blank'>$filename</a></td>
+				<td> <a href='Paste.php?id=$session_id&file=$file_path'  class='imp-style'>Copier & coller</a></td>
+			</tr>";
 		}
 	}
 	?>
+	</table>
 
-	<a href="Upload_page.php?id=<?php echo $session_id?>">Upload</a>
 </body>
+
 </html>
